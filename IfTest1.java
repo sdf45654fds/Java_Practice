ed19f32df3047bfcfddde357a26700d6e9049cfb
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-08 15:14:58 +0900

    Add files via upload

--- IfTest1.java
+++ IfTest1.java
@@ -1,3 +1,21 @@
+/*
+제어문
+   1. if 문
+   
+       - 형식 : 
+       
+	      stmt0;
+		  if(조건문){
+		      //조건문 -->   논리형데이타가 반환되는 연산 혹은 논리형변수(데이타)
+			  stmt1;
+		   }else{
+		      stmt2;
+		   }
+		   stmt3;
+
+		   조건데이타가 true인경우   stmt0-->stmt1-->stmt3;
+		   조건데이타가 false인경우  stmt0-->stmt2-->stmt3;
+ */
 
 public class IfTest1 {
 	/*
@@ -6,9 +24,9 @@ public class IfTest1 {
 	 */
 
 	public static void main(String[] args) {
-		System.out.println("main black start");
+		System.out.println("main block start");
 		System.out.println("stmt1");
-		boolean condition = true;
+		boolean condition = false;
 
 		if (condition) {
 			/*
@@ -47,12 +65,8 @@ public class IfTest1 {
 		System.out.println("stmt15");
 		System.out.println("main block end");
 		/*
-		 * return
-		 *   - 현재 return문을 실행하는 실행흐름을 
-		 *     실행흐름을만들어서 호출한곳으로 반환한다.
-		 *   - return문 생략가능
+		 * return - 현재 return문을 실행하는 실행흐름을 실행흐름을만들어서 호출한곳으로 반환한다. - return문 생략가능
 		 */
 		return;
-
 	}
 }
