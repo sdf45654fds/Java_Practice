6a091b6dacad0ce0b170afd5f2cec10108631022
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-08 19:59:12 +0900

    Update (~)ForStar.java

--- 과제/(~)ForStar.java
+++ 과제/(~)ForStar.java
@@ -26,9 +26,9 @@ public static void main(String[] args) {
 	 for (int i = 0; i < 5; i++) {
          //세로 5칸에 입력할 for문
            for (int j = 0; j <= i; j++) {
-                //별(*)문자를 가로로 증가시키는 반복문
-               System.out.print("*");
-                       //강제개행을 하지 않고 출력시키게 하는게 포인트입니다.
+  	 //별(*)문자를 가로로 증가시키는 반복문
+               System.out.print("★");
+         //강제개행을 하지 않고 출력시키게 하는게 포인트입니다.
                       
            }
            System.out.println("");        
@@ -38,7 +38,7 @@ public static void main(String[] args) {
 	for (int i = 5; i > 0; i--) {
         //처음에 5개를 찍고 1개씩 감소하는 반복문입니다.
             for (int j = 0; j < i; j++) {
-                System.out.print("*");
+                System.out.print("★");
                        //i값 개수에 따라서 별의 개수가 변하도록 합니다.
             }
             System.out.println();        
