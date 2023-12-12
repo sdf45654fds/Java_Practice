e9c253547c977d9e3e11964e004a76168cedab63
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-12 16:09:17 +0900

    Add files via upload

---
+++ CarMemberMethodMain.java
@@ -0,0 +1,53 @@
+
+public class CarMemberMethodMain {
+
+	public static void main(String[] args) {
+		/*
+		 * 1.차량번호 1234번차량 12시입차
+		 */
+		Car car1=new Car();
+		/*
+		 * 입차시데이타(번호,입차시간)대입메쏘드호출
+		 */
+		 car1.setIpChaData("1234",12);
+		 
+		 
+		 
+		/*
+		 * 2.car1 차량 16시출차
+		 */
+		/*
+		 * 2-1.출차시간 대입
+		 */
+		car1.setOutTime(6);
+		
+		/*
+		 * 2-2.주차요금계산메쏘드 호출
+		 */
+		car1.calculateFee();
+		/*
+		 * 2-3.주차요금영수증 출력메쏘드호출
+		 */
+		car1.print();
+		
+		
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
