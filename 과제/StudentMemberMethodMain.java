a7804c19fee536adb87fad9b1b94f54fc3d9e1d6
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-12 23:17:44 +0900

    Update StudentMemberMethodMain.java

--- 과제/StudentMemberMethodMain.java
+++ 과제/StudentMemberMethodMain.java
@@ -16,19 +16,18 @@ public class StudentMemberMethodMain {
 		student2.data(2,"Lee",50,95,60);
 		student3.data(3,"Bak",85,90,100);
 		
-		/*
-		 학생총점계산 메쏘드 호출(3명)
-		 학생평균계산 메쏘드 호출(3명)
-		 학생평점계산 메쏘드 호출(3명)
-		 */
+		
+		//학생총점계산 메쏘드 호출(3명)
 		student1.tot();
 		student2.tot();
 		student3.tot();
 		
+		//학생평균계산 메쏘드 호출(3명)
 		student1.avg();
 		student2.avg();
 		student3.avg();
-
+		
+		//학생평점계산 메쏘드 호출(3명)
 		student1.grade();
 		student2.grade();
 		student3.grade();
