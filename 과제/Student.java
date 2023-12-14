f159394e82187243a6dbbf85c5b922637745c00a
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-14 20:49:39 +0900

    Add files via upload

--- 과제/Student.java
+++ 과제/Student.java
@@ -1,79 +1,77 @@
-package com.itwill01.method;
-/*
-#### 성적처리를 위한 필요한 학생객체를 만들기 위한 클래스
-*/
+package com.itwill02.constructor;
+
 public class Student {
 
-	/*
- * << 속성(멤버변수)   >> 번호 이름 국어 영어 수학 총점 평균 평점 석차
- * << 기능(멤버메쏘드) >> 기본데이타입력,총점계산,평균계산,평점계산, 출력
- */
+	// 캡슐화를 하세요
+	private int no;
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	private int tot;
+	private double avg;
+	private char grade;
+	private int rank;
 
-//멤버변수 선언
-int no; //학번,gkr
-String name; //이름,dl
-int kor; //국어,rnr
-int eng; //영어,dud
-int math; //수학,tn
-int tot; //총점,chd
-double avg; //평점,vud
-char grade; //학점,gkr
-int rank; //석차
+	/* <<생성자메쏘드 선언[오버로딩]>>
+	   - 인자 0개짜리 생성자(기본생성자선언) */
+	public Student() {
 
- // 멤버메쏘드 선언
+	}
 
- //기본데이타(번호,이름,국어,영어,수학)멤버변수에대입하는메쏘드
- 
-	public void data(int no,String name,int kor, int eng, int math){
-		this.no = no;		
+	// 인자 5개짜리(번호 이름 국어 영어 수학) 생성자선언
+	public Student(int no, String name, int kor, int eng, int math) {
+		this.no = no;
 		this.name = name;
 		this.kor = kor;
 		this.eng = eng;
 		this.math = math;
-	} 
-	
-	//총점계산메쏘드
-	public void tot(){
-		this.tot = kor+eng+math;
-		return;
 	}
-	
-	//평균계산메쏘드
-	public void avg(){
-		this.avg = tot/3;
-		return;
-	}
-	
-	//평점계산메쏘드
-	//평점(A(90~100),B(80~89),C(70~79),D(60~69),F(0~59))
-	public void grade(){
-		
-		if (avg >= 90 && avg <= 100) {
-			grade = 'A';
-		} else if (avg >= 80 && avg < 90) {
-			grade = 'B';
-		} else if (avg >= 70 && avg < 80) {
-			grade = 'C';
-		} else if (avg >= 60 && avg < 70) {
-			grade = 'D';
-		} else {
-			grade = 'F';
-		}
-		return;
-	}
-	
-	//출력메쏘드
-	
-	public void Header(){
-		System.out.printf("--------------------------------------------\n");
-		System.out.printf("%s %s %s %s %s  %s %s  %s %s\n","학번","이름","국어","영어","수학","총점","평점","학점","석차");
-		System.out.printf("--------------------------------------------\n");
-	}
-	public void Print(){
-		System.out.printf("%2s %4s %2d %3d %3d %4d %3.1f %2c \n",no,name,kor,eng,math,tot,avg,grade);
-	
 
+	// 기본데이타(번호,이름,국어,영어,수학)멤버변수에대입하는메쏘드
+	public void setStudentData(int no, String name, int kor, int eng, int math) {
+		this.no = no;
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
 	}
-	
-		
+
+	// 총점계산메쏘드
+	public void calculateTotal() {
+		this.tot = this.kor + this.eng + this.math;
+	}
+
+	// 평균계산메쏘드
+	public void calculateAvg() {
+		this.avg = (((int) ((this.tot / 3.0) * 100.0)) / 100.0);
+	}
+
+	// 평점계산메쏘드
+	public void calculateGrade() {
+		if (this.avg >= 90)
+			this.grade = 'A';
+		else if (this.avg < 90 && this.avg >= 80)
+			this.grade = 'B';
+		else if (this.avg < 80 && this.avg >= 70)
+			this.grade = 'C';
+		else if (this.avg < 70 && this.avg >= 60)
+			this.grade = 'D';
+		else
+			this.grade = 'F';
+	}
+
+	// header출력메쏘드
+	public void headerPrint() {
+		System.out.printf("---------------학생 성적출력-------------------%n");
+		System.out.printf("%s %s %s %s %s %s %3s %s %s%n", "학번", "이름", "국어", "영어", "수학", "총점", "평균", "평점", "석차");
+		System.out.printf("-----------------------------------------------%n");
+	}
+
+	// 학생정보출력메쏘드
+	public void print() {
+		System.out.printf("%3d %5s %4d %4d %4d %4d %5.1f %3c %4d%n", this.no, this.name, this.kor, this.eng, this.math,
+				this.tot, this.avg, this.grade, this.rank);
+	}
+
 }
