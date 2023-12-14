0d633fff061e5f4384060b1656f1743e75d7fc05
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-14 12:03:56 +0900

    Update DefaultConstructor.java

--- 생성자/DefaultConstructor.java
+++ 생성자/DefaultConstructor.java
@@ -1,4 +1,32 @@
 
 public class DefaultConstructor {
+	/*
+	- 생성자를 하나도 정의하지않을경우 컴파일러는 디폴트(기본)생성자를 자동으로 추가한다.
+	- 생성자를 하나라도     정의할경우 컴파일러는 디폴트(기본)생성자를 자동으로 추가하지않는다. 
+	 */
+	/*
+	public DefaultConstructor() {}
+	*/
+	public DefaultConstructor() {
+	}
+
+	public DefaultConstructor(int a) {
+
+	}
+}
+
+
+
+
+-----------------------------------------------------------
+  
+public class DefaultConstructorMain {
+
+	public static void main(String[] args) {
+		DefaultConstructor defaultConstructor1 = new DefaultConstructor();
+		System.out.println(defaultConstructor1);
+		DefaultConstructor defaultConstructor2 = new DefaultConstructor(123);
+		System.out.println(defaultConstructor2);
+	}
 
 }
