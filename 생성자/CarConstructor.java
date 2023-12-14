2ed9d7c76dfc0ee5f89f831528b73ac1cfe901d2
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-14 12:13:27 +0900

    Create CarConstructor.java

---
+++ 생성자/CarConstructor.java
@@ -0,0 +1,29 @@
+
+public class CarConstructorMain {
+
+	public static void main(String[] args) {
+		System.out.println("--------------car1--------------");
+		Car car1 = new Car();
+		car1.setIpChaData("1234", 12);
+		car1.setOutTime(14);
+		car1.calculateFee();
+		car1.print();
+		System.out.println("--------------car2--------------");
+		Car car2 = new Car("4567", 13);
+		car1.setOutTime(15);
+		car1.calculateFee();
+		car1.print();
+
+		System.out.println("******오늘주차장이용차량출력******");
+		Car cara = new Car("1234", 1, 2, 1000);
+		Car carb = new Car("5678", 12, 17, 5000);
+		Car carc = new Car("3478", 6, 9, 3000);
+		Car card = new Car("2398", 3, 7, 4000);
+		cara.headerPrint();
+		cara.print();
+		carb.print();
+		carc.print();
+		card.print();
+	}
+
+}
