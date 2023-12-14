802af61834ad721c19bb75d42d9d0404a5b9e87e
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-14 23:49:18 +0900

    Update Student.java

--- 과제/Student.java
+++ 과제/Student.java
@@ -3,15 +3,15 @@ package com.itwill02.constructor;
 public class Student {
 
 	// 캡슐화를 하세요
-	private int no;
-	private String name;
-	private int kor;
-	private int eng;
-	private int math;
-	private int tot;
-	private double avg;
-	private char grade;
-	private int rank;
+	private int no; //학번
+	private String name; //이름
+	private int kor; //국어
+	private int eng; //영어
+	private int math; //수학
+	private int tot; //총점
+	private double avg; //평균
+	private char grade; //학점
+	private int rank; //석차
 
 	/* <<생성자메쏘드 선언[오버로딩]>>
 	   - 인자 0개짜리 생성자(기본생성자선언) */
@@ -74,4 +74,76 @@ public class Student {
 				this.tot, this.avg, this.grade, this.rank);
 	}
 
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getTot() {
+		return tot;
+	}
+
+	public void setTot(int tot) {
+		this.tot = tot;
+	}
+
+	public double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+
+	public char getGrade() {
+		return grade;
+	}
+
+	public void setGrade(char grade) {
+		this.grade = grade;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	
 }
