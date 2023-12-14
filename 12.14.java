6bafbff788267d86523a67961e269eb67d284961
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-14 10:10:01 +0900

    Create 12.14.java

---
+++ 12.14.java
@@ -0,0 +1,31 @@
+public class Overloading { <- //2개의 클래스 하나로 간단히 보고싶을때 (굳이 안해도됨) 
+public void method() {
+		System.out.println("public void method()");
+	}
+	public void method(int a) {
+		System.out.println("public void method(int a)");
+	}
+	public void method(String a) {
+		System.out.println("public void method(int a)");
+	}
+	public void method(int a,int b) {
+		System.out.println("public void method(int a)");
+	}
+	public void method(int a, String b) {
+		System.out.println("public void method(int a)");
+	}
+} //인자가 여러개 있을떄 타입의 조합이 일치해야함
+
+public class OverloadingMain {
+
+	public static void main(String[] args) {
+    OverLoading overLoading = new OverLoading();
+		overLoading.method();
+		overLoading.method(123);
+		overLoading.method("김수한무~~");
+		overLoading.method(100, 200);
+		overLoading.method(100, "이백");
+		overLoading.method(1, 2, 3);
+		overLoading.method("일", 2, 3);
+	}
+}
