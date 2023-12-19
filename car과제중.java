3224284381762bcce8417228d84f6c33402a585a
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-19 18:12:33 +0900

    Update car과제중.java

--- car과제중.java
+++ car과제중.java
@@ -4,13 +4,13 @@ public class CarArrayMain {
 	public static void main(String[] args) {
 		// Car[] carArray=new Car[30];
 
-		Car[] carArray = { new Car("1234", 1), new Car("2345", 2), new Car("5654", 8), new Car("3456", 3),
-				new Car("4567", 4), new Car("3422", 12), new Car("5678", 5), new Car("6789", 7), new Car("7891", 18),
-				new Car("1112", 13), new Car("1314", 14), new Car("1516", 15), new Car("1718", 16), new Car("1920", 17),
-				new Car("7789", 11), new Car("1120", 10), new Car("2122", 19), new Car("2324", 20), new Car("2526", 21),
-				new Car("2728", 22), new Car("2930", 23), new Car("2389", 9), new Car("3132", 24), new Car("3334", 25),
-				new Car("3536", 26), new Car("3738", 27), new Car("3940", 28), new Car("4142", 29), new Car("4344", 30),
-				new Car("5555", 6) };
+		Car[] carArray = Car[] carArray= {
+				null,null,new Car("5654", 8),null,null,
+				new Car("3422", 12),null,null,null,null,
+				null,null,null,null,new Car("7789", 11),
+				new Car("1120", 10),null,null,null,null,
+				null,new Car("2389", 9),null,null,null,
+				null,null,null,null,new Car("5555", 6)
 		/*
 		null.메쏘드() --> NullPointerException
 		 */
