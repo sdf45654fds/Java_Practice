8b9beed0907e0451eff1a062257260867599082b
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-20 01:24:47 +0900

    Update car과제중.java

--- car과제중.java
+++ car과제중.java
@@ -4,25 +4,52 @@ public class CarArrayMain {
 	public static void main(String[] args) {
 		// Car[] carArray=new Car[30];
 
-		Car[] carArray = Car[] carArray= {
+		Car[] carArray = {
 				null,null,new Car("5654", 8),null,null,
 				new Car("3422", 12),null,null,null,null,
 				null,null,null,null,new Car("7789", 11),
 				new Car("1120", 10),null,null,null,null,
 				null,new Car("2389", 9),null,null,null,
 				null,null,null,null,new Car("5555", 6)
-		};
+				};
 	
 	/*
 	null.메쏘드() --> NullPointerException
 	 */
 	System.out.println("1.전체 차량출력");
 	Car.headerPrint();
+	for (int a = 0; a < carArray.length; a++) {
+		if (carArray[a] != null) {
+			carArray[a].print();
+		}
+	}
 
 	System.out.println("2.전체주차구획수");
-
+	System.out.println(carArray.length);
+	
 	System.out.println("3.남은주차구획수");
+	int left = 0;
+	for (int a = 0; a < carArray.length; a++) {
+		if (carArray[a] == null) {
+			left++;
+		}
 
+	}
+	System.out.println("남은주차구획수 : "+left);
+	
+	System.out.println("4.입차 (차량번호 7777번 9시입차) ");
+	/*
+	 * 1.차량객체생성
+	 * 2.빈자리찾아서대입
+	 */
+	Car car1 = new Car("7777", 9);
+	for (int a = 0; a < carArray.length; a++) {
+		if (carArray[a] == null) {
+			carArray[a] = car1;
+			break;
+		}
+
+	}
 	
 	System.out.println("4.입차");
 	
@@ -30,25 +57,52 @@ public class CarArrayMain {
 	 * 1.차량객체생성
 	 * 2.빈자리찾아서대입
 	 */
+	for (int a = 0; a < carArray.length; a++) {
+		if (carArray[a] != null) {
+			if (carArray[a].getNo().equals("7789")) {
+				carArray[a].print();
+			}
+		}
+
+	}
 	
 	System.out.println("5.차량번호 7789번  차한대 정보출력");
+	for (int a = 0; a < carArray.length; a++) {
+		if (carArray[a] != null) {
+			if (carArray[a].getNo().equals("7789")) {
+				carArray[a].print();
+			}
+		}
 
+	}
 	System.out.println("6.입차시간 10시이후 차량여러대 찾아서 정보출력");
-
+	for (int a = 0; a < carArray.length; a++) {
+		if (carArray[a] != null) {
+			if (carArray[a].getInTime() >= 10) {
+				carArray[a].print();
+			}
+		}
+	}
 	System.out.println("7.2389번차량 12시 출차");
-
+	
 	/*
 	 * 1. 2389번차량찾기
 	 * 2. 출차
 	 * 3. 영수증출력
 	 * 4. 2389번차량 주차장에서 차량삭제
 	 */
-	carArray[21]=null;System.out.println("99. 2389번차량 출차후전체 차량출력");Car.headerPrint();for(
+	for (int a = 0; a < carArray.length; a++) {
+		if (carArray[a] != null) {
+			
+		}
 
-	int i = 0;i<carArray.length;i++)
+	}
+	//carArray[21]=null
+	carArray[21]=null;System.out.println("99. 2389번차량 출차후전체 차량출력");Car.headerPrint();for(
+	int a = 0; a<carArray.length;a++)
 	{
-			if(carArray[i]!=null) {
-				carArray[i].print();
+			if(carArray[a]!=null) {
+				carArray[a].print();
 			}
 		}
 	}
