16b48a3f724e8bf55360360692daf8f306b1af1a
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-19 18:05:13 +0900

    Create car과제.java

---
+++ car과제.java
@@ -0,0 +1,15 @@
+package com.itwill04.array.car;
+
+public class CarArrayMain {
+	public static void main(String[] args) {
+		// Car[] carArray=new Car[30];
+
+		Car[] carArray = { new Car("1234", 1), new Car("2345", 2), new Car("5654", 8), new Car("3456", 3),
+				new Car("4567", 4), new Car("3422", 12), new Car("5678", 5), new Car("6789", 7), new Car("7891", 18),
+				new Car("1112", 13), new Car("1314", 14), new Car("1516", 15), new Car("1718", 16), new Car("1920", 17),
+				new Car("7789", 11), new Car("1120", 10), new Car("2122", 19), new Car("2324", 20), new Car("2526", 21),
+				new Car("2728", 22), new Car("2930", 23), new Car("2389", 9), new Car("3132", 24), new Car("3334", 25),
+				new Car("3536", 26), new Car("3738", 27), new Car("3940", 28), new Car("4142", 29), new Car("4344", 30),
+				new Car("5555", 6) };
+	}
+}
