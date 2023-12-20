986d67aef89009524c2dcc808f6206d1055873db
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-20 17:43:15 +0900

    Update MemberFieldMain.java

--- Quiz/12.19-2/MemberFieldMain.java
+++ Quiz/12.19-2/MemberFieldMain.java
@@ -6,16 +6,24 @@ public class MemberFieldMain {
 		/*
 		 * 1.MemberField객체생성
 		 */
-		
-		
+		MemberField mf = new MemberField();
+
 		/*
 		 * 2.MemberField객체의 멤버변수4개에 데이타대입
 		 */
-		
-		
+		mf.a = 1;
+		mf.b = 'B';
+		mf.c = 3.3;
+		mf.d = "DD";
+
 		/*
 		 * 3.MemberField객체의 4개멤버변수내용출력
 		 */
+		System.out.println("a = " + mf.a);
+		System.out.println("b = " + mf.b);
+		System.out.println("c = " + mf.c);
+		System.out.println("d = " + mf.d);
+
 	}
 
 }
