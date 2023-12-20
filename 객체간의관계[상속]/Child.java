34e975259dc4a5e61e239f0a84a5e4ccf0d9c57d
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-20 14:20:00 +0900

    Add files via upload

---
+++ 객체간의관계[상속]/Child.java
@@ -0,0 +1,19 @@
+
+public class Child extends Parent {
+	/*
+	public int member1;
+	public int member2;
+	public void method1() {
+		System.out.println("Parent.method1():this-->"+this);
+	}
+	public void method2() {
+		System.out.println("Parent.method2():this-->"+this);
+	}
+	*/
+	public int member3;
+
+	public void method3() {
+		System.out.println("Child.method3():this-->" + this);
+	}
+
+}
---
+++ 객체간의관계[상속]/ObjectChild.java
@@ -0,0 +1,16 @@
+/*
+  5. 사용자정의 클래스들도 Object 클래스라는 최상위클래스를
+      상속  받아야한다. 
+ */
+public class ObjectChild /*extends Object*/ {
+	/*
+	 * extends Object  는 생략가능
+	 */
+	/*
+	 << Object >>
+	  - Class Object is the root of the class hierarchy.
+	  - Every class has Object as a superclass. 
+	  - All objects, including arrays, implement the methods of this class.
+	 */
+
+}
