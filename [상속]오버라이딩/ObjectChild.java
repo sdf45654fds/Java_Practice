266f7738d40919993340b0d063bd4cdb1bd85b44
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-21 14:19:20 +0900

    Add files via upload

---
+++ [상속]오버라이딩/ObjectChild.java
@@ -0,0 +1,13 @@
+
+public class ObjectChild {
+	/*
+	  public String toString(){
+	  		return "ObjectChild@ad3412";
+	  }
+	 */
+	@Override
+	public String toString() {
+		return "난 오브젝트자식객체다.!!!";
+
+	}
+}
